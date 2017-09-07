b561d9aa0c271bbd13cc8623c4b7d130a4262034
mgkOV <myagkov.mww@gmail.com>
2017-09-07 16:55:32 +0300

    add action to navigation container

--- app/containers/NavigationContainer/index.js
+++ app/containers/NavigationContainer/index.js
@@ -8,9 +8,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import selectNavigationContainer from './selectors';
 import Navigation from '../../components/Navigation';
+import { requestTopics } from './actions';
 
 export class NavigationContainer extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    requestTopics: React.propTypes.func.isRequired
+  };
   render() {
     return (
       <div>
